23f0144b1d98d7d641f136a1106ab558af3a7f2c
Kasper Wissendorf <kw2011@live.dk>
2025-10-23 22:05:07 +0200

    revert back to old inject

--- src/extension/index.ts
+++ src/extension/index.ts
@@ -1,20 +1,23 @@
-(function (): void {
-	const injectScript = (file: string): void => {
-		const s: HTMLScriptElement = document.createElement("script");
-		s.src = chrome.runtime.getURL(file);
-		s.onload = () => s.remove();
-		(document.head || document.documentElement).append(s);
-	}
+function injectScript(file: string): void {
+	const s: HTMLScriptElement = document.createElement("script");
+	s.src = chrome.runtime.getURL(file);
+	s.onload = () => s.remove();
+	(document.head || document.documentElement).append(s);
+}
 
-	const injectStyle = (file: string): void => {
-		const s: HTMLLinkElement = document.createElement("link");
-		s.rel = "stylesheet";
-		s.href = chrome.runtime.getURL(file);
-		(document.head || document.documentElement).append(s);
-	}
+function injectStyle(file: string): void {
+	const s: HTMLLinkElement = document.createElement("link");
+	s.rel = "stylesheet";
+	s.href = chrome.runtime.getURL(file);
+	(document.head || document.documentElement).append(s);
+}
 
-	injectStyle("dist/react.css");
-	injectScript("dist/showdown.js");
+const INJECT_DELAY = 250; // milliseconds
+
+injectScript("dist/showdown.js");
+injectStyle("dist/react.css");
+setTimeout(() => {
+	console.log(`Injecting React and styles after ${(INJECT_DELAY / 1000).toFixed(2)} seconds delay`);
 	injectScript("dist/lib-react.js");
 	injectScript("dist/react.js");
-})();
+}, INJECT_DELAY);
